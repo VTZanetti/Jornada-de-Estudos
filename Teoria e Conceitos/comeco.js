0c33c1cbcff528f8f776ef5216bb1658e558d7ce
Vitor Zanetti <vitorzanetticontatos@outlook.com>
2025-05-04 04:51:36 -0300

    Um novo ciclo de estudos

---
+++ Teoria e Conceitos/comeco.js
@@ -0,0 +1,23 @@
+{ // bloco de cod #01
+    console.log("Passo #01"); // sentenca
+    console.log("Passo #02"); 
+    console.log("Passo #03");
+}
+
+{ // bloco de cod #02
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+
+{ // bloco de cod #03
+    ;
+    ;
+    ;
+}
+
+console.log("Fim!");
+
+/*
+Organizacao de codigos
+-> Pastas -> Arquivos (.c/ .py/ .cpp/ .js/ etc) -> Sentencas/Codigos
+*/
---
+++ Teoria e Conceitos/desafio_sentenca_bloco.js
@@ -0,0 +1,35 @@
+// Arquivos que de aprendizado entram Bloco, comentarios, sentenca 1 e 2 de conhecimento
+
+
+    // Resultado precisa ser:
+    // Bloco 1
+    // 1
+    // 3
+    // 5
+    // 7
+    // 9
+    // Bloco 2
+    // 2
+    // 4
+    // 6
+    // 8
+    // 10
+
+    //Numeros impares
+    
+    {
+        console.log(1);
+        console.log(3);
+        console.log(5);
+        console.log(7);
+        console.log(9);
+    }
+
+        //Numeros pares
+    {
+        console.log(2);
+        console.log(4);
+        console.log(6);
+        console.log(8);
+        console.log(10);
+    }
