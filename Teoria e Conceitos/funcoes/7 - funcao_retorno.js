e6397c61ed3206cb889df0fb32128337997cc58b
Vitor Zanetti <vitorzanetticontatos@outlook.com>
2025-05-12 18:40:16 -0300

    Dia 9

---
+++ Teoria e Conceitos/funcoes/7 - funcao_retorno.js
@@ -0,0 +1,18 @@
+function sempreRetornaUm () {
+return 1;
+}
+
+function textoOuNumero (retornaTexto) {
+    if(retornaTexto){
+        return "Sou um texto!";
+       } else {
+            return 123;
+        }
+    }
+
+let valor = sempreRetornaUm();
+
+console.log(valor)
+
+let texto = textoOuNumero(true);
+console.log(texto);
